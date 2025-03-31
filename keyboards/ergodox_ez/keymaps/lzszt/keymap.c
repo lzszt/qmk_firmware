8b7ee6d134048a5bcbba06148243faf577b4e75b
Felix Leitz <felix.leitz@active-group.de>
2025-03-31 07:53:44 +0200

    Add toggle for german umlauts

--- keyboards/ergodox_ez/keymaps/lzszt/keymap.c
+++ keyboards/ergodox_ez/keymaps/lzszt/keymap.c
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TRNS,     KC_Z,    KC_U,    KC_I,    KC_O,              KC_P,           KC_BSLS,
   KC_H,         KC_J,    KC_K,    KC_L,    LT(UML, KC_SCLN), GUI_T(KC_QUOT),
   MEH_T(KC_NO), KC_N,    KC_M,    KC_COMM, KC_DOT,            CTL_T(KC_SLSH), KC_RSFT,
-  KC_UP,        KC_DOWN, KC_LBRC, KC_RBRC, KC_TRNS,
+  KC_UP,        KC_DOWN, KC_LBRC, KC_RBRC, TT(UML),
   KC_LALT, KC_ESC,
   KC_PGUP,
   KC_PGDN, KC_TAB, KC_ENT
